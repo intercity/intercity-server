3f3739275e467437fec5bd02dafd1efc31069653
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2016-08-23 15:56:53 +0200

    We no longer wants to have the FROM_EMAIL in the config file
    Depends on: https://github.com/intercity/intercity-next/pull/172

--- lib/intercity_server/installer.rb
+++ lib/intercity_server/installer.rb
@@ -3,7 +3,7 @@ require "highline"
 
 module IntercityServer
   class Installer
-    attr_reader :hostname, :from_email
+    attr_reader :hostname
 
     def self.execute
       Installer.new.execute
@@ -16,11 +16,6 @@ module IntercityServer
         q.validate = hostname_regex
       end
       cli.say "Hostname is set to #{@hostname}"
-
-      @from_email = cli.ask("What email should we use as sender (For example for activtion emails)") do |q|
-        q.validate = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/
-      end
-      cli.say "From email is set to #{@from_email}"
 
       cli.say "---- Installing docker"
       install_docker
@@ -58,7 +53,6 @@ module IntercityServer
       config_file = "/var/intercity/containers/app.yml"
       config_content = File.read config_file
       config_content = config_content.gsub(/intercity\.example\.com/, hostname)
-      config_content = config_content.gsub(/example\@example.com/, from_email)
 
       File.open(config_file, "w") {|file| file.puts config_content }
     end
